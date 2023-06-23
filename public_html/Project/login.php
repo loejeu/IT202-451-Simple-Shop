a701ca1892929a08b1e1ac7db59070dae8bbbf89
loejeu <joe.leung020@gmail.com>
2023-06-23 16:45:28 -0400

    Milestone1 housekeeping

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . "/../../partials/nav.php");
+require(__DIR__ . "/../../partials/nav.php");
 ?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
@@ -21,40 +21,43 @@ require_once(__DIR__ . "/../../partials/nav.php");
     }
 </script>
 <?php
- //TODO 2: add PHP Code
- if(isset($_POST["email"]) && isset($_POST["password"])) {
-    $email = se($_POST,"email", "", false); //$_POST["email"];
-    $password = se($_POST,"password", "", false); //$_POST["password"];
+//TODO 2: add PHP Code
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+    $email = se($_POST, "email", "", false);
+    $password = se($_POST, "password", "", false);
 
-    //TODO 3: validate/use
+    //TODO 3
     $hasError = false;
-    if(empty($email)) {
-        flash("Email must be provided <br>");
+    if (empty($email)) {
+        flash("Email must not be empty");
         $hasError = true;
     }
     //sanitize
     //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
     $email = sanitize_email($email);
-    /*if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        flash("Please enter a valid email <br>");
+    //validate
+    /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        flash("Invalid email address");
         $hasError = true;
     }*/
-    if(!is_valid_email($email)) {
-        flash("Please enter a valid email <br>");
+    if (!is_valid_email($email)) {
+        flash("Invalid email address");
         $hasError = true;
     }
-    if(empty($password)) {
-        flash("Password must be provided <br>");
+    if (empty($password)) {
+        flash("password must not be empty");
         $hasError = true;
     }
-    if(strlen($password) < 8) {
-        flash("Password must be at least 8 characters long <br>");
+    if (!is_valid_password($password)) {
+        flash("Password too short");
         $hasError = true;
     }
-    if(!$hasError) {
+    if (!$hasError) {
+        //flash("Welcome, $email");
         //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email");
+        $stmt = $db->prepare("SELECT id, email, username, password from Users 
+        where email = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
             if ($r) {
@@ -63,8 +66,9 @@ require_once(__DIR__ . "/../../partials/nav.php");
                     $hash = $user["password"];
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
-                        flash("Welcome $email");
-                        $_SESSION["user"] = $user;
+                        //flash("Weclome $email");
+                        $_SESSION["user"] = $user; //sets our session data from db
+                        flash("Welcome, " . get_username());
                         die(header("Location: home.php"));
                     } else {
                         flash("Invalid password");
@@ -77,6 +81,7 @@ require_once(__DIR__ . "/../../partials/nav.php");
             flash("<pre>" . var_export($e, true) . "</pre>");
         }
     }
- }
+}
 ?>
-<?php require_once(__DIR__ . "/../../partials/flash.php");
+<?php 
+require(__DIR__."/../../partials/flash.php");
